e48f320e207af78a293ad9f00e9f87b91e856bf6
mynulislam75 <mynulislam181120@gmail.com>
2023-09-12 21:40:20 +0600

    basic operator

---
+++ operator.js
@@ -0,0 +1,43 @@
+//LETSSS EXPLORE BASIC OPERATOR
+
+//ADDITION
+function myFunction (a,b){
+let result=a+b;
+return result;
+}
+let resultIs=myFunction(10,20);
+console.log(resultIs)
+
+//SUBTRACTION
+const myFuntionTwo=(a,b)=>{
+    let subtraction=a-b;
+    return subtraction;
+}
+let subResult=myFuntionTwo(10,30);
+console.log(subResult);
+
+//MULTIPLICATION
+let am=93;
+let bm=10;
+const multiplication=(c,d)=>{
+    let mulResult=c*d;
+    return mulResult;
+}
+let resultM=multiplication(am,bm);
+console.log(resultM);
+
+//DIVISION
+const divFunction=(c,d)=>{
+    let divCal=c/d;
+    return divCal;
+}
+let divResult=divFunction(am,bm);
+console.log(divResult);
+
+//REMAINDER
+const remainFunction=(c,d)=>{
+    let remainderCal=c%d;
+    return remainderCal;
+}
+let remainderResult=remainFunction(am,bm);
+console.log(remainderResult);
