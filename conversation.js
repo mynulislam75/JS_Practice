1bef9b030f9d19797b881122636f6c7dd61fc283
mynulislam75 <mynulislam181120@gmail.com>
2023-09-12 15:34:32 +0600

    first commit

---
+++ conversation.js
@@ -0,0 +1,29 @@
+//STRING CONVERSATION
+
+let number=1234;
+console.log(number);
+let conversationValue=String(number);
+let see=typeof(conversationValue)
+let see2=typeof(number)
+let add=conversationValue+conversationValue;
+let add2=number+number;
+console.log(add);
+console.log(add2)
+console.log(see)
+
+//NUMBER CONVERSATION
+let stringIs="4";
+let stringIs2="6";
+let add3=stringIs+stringIs2;
+console.log(add3);
+let numNow=Number(stringIs);
+let numNow2=Number(stringIs2);
+let add4=numNow+numNow2;
+console.log(add4);
+
+//BOOLEAN CONVERSATION
+let anyName="abcd";
+let testOne=Boolean(anyName);
+console.log(testOne);
+let testOne2;
+console.log(Boolean(testOne2))
---
+++ variable.js
@@ -0,0 +1,27 @@
+// practice JS variable STRING
+let nameIs="Mynul";
+let nameIs2="Marufa";
+let name3="Sabrina";
+let name4="Masum";
+
+console.log(nameIs);
+console.log(nameIs2);
+console.log(name3);
+console.log(name4);
+
+
+//practice Variable as NUMBER
+let num1=12333
+let num2=76767;
+var num3=76776;
+const num=8737;
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(num)
+
+//practice variable BOOLEAN
+let booleanIs=true;
+var boolean2=false;
+console.log(boolean2);
+console.log(booleanIs)
