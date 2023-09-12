ff184df9afa6499a7ea78673de7b21157c62dc81
mynulislam75 <mynulislam181120@gmail.com>
2023-09-12 23:30:56 +0600

    unary operator

--- operator.js
+++ operator.js
@@ -1,43 +1,55 @@
 //LETSSS EXPLORE BASIC OPERATOR
 
 //ADDITION
-function myFunction (a,b){
-let result=a+b;
-return result;
+function myFunction(a, b) {
+    let result = a + b;
+    return result;
 }
-let resultIs=myFunction(10,20);
+let resultIs = myFunction(10, 20);
 console.log(resultIs)
 
 //SUBTRACTION
-const myFuntionTwo=(a,b)=>{
-    let subtraction=a-b;
+const myFuntionTwo = (a, b) => {
+    let subtraction = a - b;
     return subtraction;
 }
-let subResult=myFuntionTwo(10,30);
+let subResult = myFuntionTwo(10, 30);
 console.log(subResult);
 
 //MULTIPLICATION
-let am=93;
-let bm=10;
-const multiplication=(c,d)=>{
-    let mulResult=c*d;
+let am = 93;
+let bm = 10;
+const multiplication = (c, d) => {
+    let mulResult = c * d;
     return mulResult;
 }
-let resultM=multiplication(am,bm);
+let resultM = multiplication(am, bm);
 console.log(resultM);
 
 //DIVISION
-const divFunction=(c,d)=>{
-    let divCal=c/d;
+const divFunction = (c, d) => {
+    let divCal = c / d;
     return divCal;
 }
-let divResult=divFunction(am,bm);
+let divResult = divFunction(am, bm);
 console.log(divResult);
 
 //REMAINDER
-const remainFunction=(c,d)=>{
-    let remainderCal=c%d;
+const remainFunction = (c, d) => {
+    let remainderCal = c % d;
     return remainderCal;
 }
-let remainderResult=remainFunction(am,bm);
+let remainderResult = remainFunction(am, bm);
 console.log(remainderResult);
+
+//UNARY OPERATION
+
+let stringONe = "121";
+let stringTwo = "231";
+
+const stringFunction = (c, d) => {
+ let r=+stringONe+ +stringTwo;
+ return r;
+}
+let resultOfS=stringFunction(stringONe,stringTwo);
+console.log(resultOfS);
